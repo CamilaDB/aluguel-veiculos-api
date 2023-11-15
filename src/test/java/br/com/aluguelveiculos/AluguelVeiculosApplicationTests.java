374a415df40f53fb58e8d002523a065254bbdaf5
CamilaDB <cbazzo@hotmail.com>
2023-11-14 22:23:31 -0300

    create project

---
+++ src/test/java/br/com/aluguelveiculos/AluguelVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aluguelveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
